6a0ccf3840a71ecc7b25f8429ff7251425a4ea4f
Carly Donais <carly.donais@gmail.com>
2022-05-01 17:29:03 -0400

    form reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,11 @@ var taskFormHandler=function(event){
     event.preventDefault();
     var taskNameInput=document.querySelector("input[name='task-name']").value;
     var taskTypeInput=document.querySelector("select[name='task-type']").value;
-
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!")
+        return false;
+    }
+    formEL.reset();
     //package up data as an object
     var taskDataObj={
         name:taskNameInput,
@@ -14,7 +18,8 @@ var taskFormHandler=function(event){
 
     // send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
-}
+};
+
 var createTaskEl=function(taskDataObj) {
         // create list item
         var listItemEl=document.createElement("li");
@@ -23,7 +28,7 @@ var createTaskEl=function(taskDataObj) {
         // create div to hold task info and add to list item
         var taskInfoEl=document.createElement("div");
         taskInfoEl.className="task-info";
-        taskInfoEl.innerHTML= "<h3 class='task-name'>" + taskDataObj + "</h3><span class='task-type'>" + taskDataObj + "</span>";    
+        taskInfoEl.innerHTML= "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";    
         listItemEl.appendChild(taskInfoEl);
        
         // add entire list item to list
